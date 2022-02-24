c2a30fc3dfdad7fc8832260888365491e8bfb6a3
Burak baki Erdogan <berdogan@k2m13s03.42kocaeli.com.tr>
2022-02-24 07:41:28 +0300

    son

--- libft.h
+++ libft.h
@@ -5,6 +5,7 @@
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, void const *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
