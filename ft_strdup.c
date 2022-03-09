0b42a4fedbbfd3fa774784e0fb94cc7a2fabc8f9
Mithrandir-Stack <98288229+burakbakierdogan@users.noreply.github.com>
2022-03-09 14:46:32 +0300

    libft

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 14:33:33 by berdogan          #+#    #+#             */
+/*   Updated: 2022/03/09 14:33:33 by berdogan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strdup (const char *str)
+{
+	char	ptr;
+	int	i;
+
+	i = 0;
+	ptr = (char *) malloc (sizeof(char) * (ft_strlen(str) + 1));
+	if (!ptr)
+		return (NULL);
+	while (i < ft_strlen(str))
+	{
+		*((char *) ptr + i) = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
--- libft.h
+++ libft.h
@@ -8,6 +8,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, void const *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strdup (const char *str);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
