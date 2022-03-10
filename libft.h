84a8025646ece3eae0815ab9c0ce664f250b92e4
Mithrandir-Stack <98288229+burakbakierdogan@users.noreply.github.com>
2022-03-10 14:56:17 +0300

    libft

--- libft.h
+++ libft.h
@@ -30,3 +30,5 @@ char	*ft_strchr(const char *s, int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
