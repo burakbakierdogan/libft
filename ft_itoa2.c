4845ea56ac9f57454396e4ae2d0625fc05dfdd4e
Burak baki Erdogan <berdogan@k2m01s02.42kocaeli.com.tr>
2022-03-14 14:20:29 +0300

    itoa

--- ft_itoa2.c
+++ ft_itoa2.c
@@ -1,14 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_itoa2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/10 14:09:40 by berdogan          #+#    #+#             */
-/*   Updated: 2022/03/14 14:19:33 by berdogan         ###   ########.fr       */
+/*   Created: 2022/03/14 12:57:44 by berdogan          #+#    #+#             */
+/*   Updated: 2022/03/14 14:10:52 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <libft.h>
 
@@ -40,33 +41,53 @@ static	char	*ft_reverse(char *str)
 		*(buffer + index++) = *(str + size--);
 	}
 	*(buffer + index) = '\0';
+	buffer = ft_reverse(buffer);
 	return (buffer);
 }
+
+static	char	*ft_putnumber(char *s, int n)
+{
+	int		number;
+	size_t	index;
+	char	*buffer;
+	
+	index = 0;
+	buffer = (char *) malloc ( sizeof(char) * ft_sizer(n) + 1);
+	if (!buffer)
+		return (NULL);
+	buffer = ft_memmove (buffer, s, ft_sizer(n)+ 1);
+	number = n;
+	if (number < 0)
+		n *= -1;
+	while (number > 0)
+	{
+		*((char *) buffer + index++) = (number % 10) + 48;
+		number /= 10;
+	}
+	if (n < 0)
+		*((char *) buffer + index++) = '-';
+	*((char *) buffer + index) = '\0'; 
+	return (buffer);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		i;
-	int		number;
+	int	i;
+	int number;
 
 	i = 0;
 	number = n;
-	str = (char *) malloc (ft_sizer(n) +1 * sizeof(char));
-	if (!str)
-		return (NULL);
+	if (!(str = (char *) malloc
+		(ft_sizer(n) +1 * sizeof(char))))
+			return (NULL);
 	if (n == 0)
 	{
-		str = "0";
+		str[i] = 48;
+		str[i+1] = '\0';
 		return (str);
 	}
-	if (number < 0)
-		number *= -1; 
-	while (number > 0)
-	{
-		str[i++] = (number%10) + 48;
-		number /= 10;
-	}
-	if ( n < 0)
-		str[i++] = '-';
-	str[i] = '\0';
-    return (ft_reverse (str));
+	str = ft_putnumber (str, n);
+	ft_reverse(str);
+	return (str);
 }
