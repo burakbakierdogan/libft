7a25b47e6e57df24c79305e53c65aaf6d728a96a
Burak baki Erdogan <berdogan@k2m15s01.42kocaeli.com.tr>
2022-03-17 18:53:41 +0300

    ft_split.c

--- ft_split3.c
+++ ft_split3.c
@@ -6,7 +6,7 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 07:20:20 by berdogan          #+#    #+#             */
-/*   Updated: 2022/03/17 18:49:31 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:53:10 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 static	void	ft_mover(char *s, int n)
 {
-	ft_memmove(s,s+1,n);
+	ft_memmove(s, s + 1, n);
 }
 
 static	char	*ft_stringer(char const *s, char c, int n)
 {
 	char	*str;
 	int		i;
-	
+
 	i = 0;
 	str = (char *) malloc ((n + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_memmove(str, s, n+1);
+	ft_memmove(str, s, n + 1);
 	while (i < n)
 	{
 		if (str[i] == c)
@@ -34,11 +34,8 @@ static	char	*ft_stringer(char const *s, char c, int n)
 		i++;
 	}
 	i = 0;
-	
 	while (*(str) == '\0')
 		str++;
-	i = 0;
-
 	while (i < n)
 	{
 		if (str[i] == '\0' && str[i + 1] == '\0')
@@ -55,7 +52,7 @@ char	**ft_split(char const *s, char c)
 	int		size;
 	int		i;
 	int		j;
-	
+
 	i = 0;
 	j = 0;
 	size = ft_strlen(s);
@@ -66,11 +63,11 @@ char	**ft_split(char const *s, char c)
 	parr[j++] = str;
 	while (i < size)
 	{
-		if (str[i] == '\0' && str[i+1] != '\0')
-			{
-				parr[j] = str + i+1;
-				j++;
-			}
+		if (str[i] == '\0' && str[i + 1] != '\0')
+		{
+			parr[j] = str + i + 1;
+			j++;
+		}
 		i++;
 	}
 	return (parr);
