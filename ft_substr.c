03189369077801804531358766278f05af1d0d8f
Burak baki Erdogan <berdogan@k2m15s01.42kocaeli.com.tr>
2022-03-21 15:40:09 +0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 02:26:51 by berdogan          #+#    #+#             */
-/*   Updated: 2022/03/14 11:49:02 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/03/21 15:39:36 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned char	*substr;
-	int				i;
+	char	*substr;
+	int		i;
 
 	i = 0;
 	if (!s || !(substr = (char *) malloc (sizeof(char) * (len + 1))))
