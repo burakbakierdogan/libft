31f5cd4e9114c4d2d8cac12a5c535daaadc00015
Burak baki Erdogan <berdogan@k2m01s02.42kocaeli.com.tr>
2022-03-31 18:54:10 +0300

    split

--- Makefile
+++ Makefile
@@ -13,11 +13,8 @@
 CC		= gcc
 CFLAGS	= -Wall -Wextra -Werror
 LIB1	= ar -rcs
-LIB2	= ranlib
 RM		= /bin/rm -f
-
 NAME	= libft.a
-
 INCLUDE	= libft.h
 SRCS	= ft_isascii.c ft_isprint.c ft_isalpha.c ft_isdigit.c ft_isalnum.c \
 			ft_tolower.c ft_toupper.c ft_strlen.c ft_strlcpy.c ft_strlcat.c \
--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,30 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 06:22:51 by berdogan          #+#    #+#             */
-/*   Updated: 2022/03/30 17:00:48 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/03/31 18:46:48 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static	int	ft_wcounter(char const *s, char c)
+{
+	int	total;
+	
+	total = 0;
+	while (*s == c && *s)
+		s++;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s)
+			total++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (total + 1);
+}
 
 static	int	ft_sizer(char const *s, char c)
 {
@@ -19,16 +38,6 @@ static	int	ft_sizer(char const *s, char c)
 	i = 0;
 	while (s[i] != c && s[i])
 		i++;
-	return (i + 1);
-}
-
-static	int	ft_sizer2(char const *s, char c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] == c)
-		i++;
 	return (i);
 }
 
@@ -36,57 +45,36 @@ static	char	*ft_new(char const *s, char c)
 {
 	char	*str;
 	int		i;
-	int		j;
-	int		k;
-
-	j = 0;
+	
 	i = 0;
-	k = ft_sizer(s, c);
-	str = (char *) ft_calloc (k, sizeof(char));
-	if (!str)
-		return (NULL);
+	str = (char *) malloc (ft_sizer(s, c) + 1 * sizeof(char));
 	while (s[i] && s[i] != c)
-			str[j++] = s[i++];
-	return (str);
-}
-
-static	int	ft_wordcount(char const *s, char c)
-{
-	size_t	i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	while (i < ft_strlen(s))
 	{
-		while (s[i] == c && i < ft_strlen(s))
-		{
-			j++;
-			i += ft_sizer2(s + i, c);
-			if (s[i] == '\0' && j != 0)
-				j--;
-		}
+		str[i] = s[i];
 		i++;
 	}
-	return (j);
+	str[i] = '\0';
+	return (str);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**parr;
-	size_t	i;
+	int		i;
 	int		j;
-
+	
 	i = 0;
 	j = 0;
-	parr = (char **) ft_calloc(ft_wordcount(s, c) + 1, sizeof(char *));
+	if (!s)
+		return (NULL);
+	parr = (char **) malloc (ft_wcounter(s, c) * sizeof(char *));
 	if (!parr)
 		return (NULL);
-	while (i < ft_strlen(s))
+	while (s[i])
 	{
-		if (s[i] == c)
+		while (s[i] == c && s[i])
 			i++;
-		while (s[i] != c && i < ft_strlen(s))
+		if (s[i])
 		{
 			parr[j++] = ft_new(s + i, c);
 			i += ft_sizer(s + i, c);
