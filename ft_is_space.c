f8bac7d540ef4adcb11b56e1b1f403807b550af6
burakbakierdogan <98288229+burakbakierdogan@users.noreply.github.com>
2022-10-03 03:38:17 +0300

    done

--- ft_is_space.c
+++ ft_is_space.c
@@ -6,15 +6,17 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 17:23:37 by berdogan          #+#    #+#             */
-/*   Updated: 2022/09/26 17:24:16 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/10/03 03:37:33 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_is_space(char c)
+int	ft_isspace(int c)
 {
-	if (c == ' ' || c == '\t')
+	if (c == ' ' || c == '\n' || c == '\t'
+		|| c == '\v' || c == '\f'
+		|| c == '\r')
 		return (1);
 	return (0);
 }
